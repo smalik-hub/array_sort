b6d039d325683f37fa9b237c9d939609a5a78d39
smalik-hub <shahzaib.malik@alumni.ubc.ca>
2019-10-15 20:36:29 -0700

    sorts an array in descending order

---
+++ array_sort_descending.cxx
@@ -0,0 +1,31 @@
+/*
+ * array_sort_descending.cxx
+ *
+ *  Created on: Sep. 17, 2019
+ *      Author: sm2016
+ */
+
+// C++ program to demonstrate descending order sort using
+// greater<>()
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+	int array[] = { 8, 6, 9, 3, 5, 2, 0, 4, 1, 10, 7 };
+
+	int n = sizeof(array) / sizeof(array[0]);
+
+	sort(array, array + n, greater<int>());
+
+	cout << "After sort in descending order: " << endl;
+
+	for (int i = 0; i < n; i++) {
+
+		cout << array[i] << " ";
+	}
+
+	return 0;
+}
+
